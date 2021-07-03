f2920ee6e88f535d71ea17b61cd58ec27a352c64
KsArT-IT <ksart.it@gmail.com>
2021-07-04 00:21:49 +0300

    feat: implement algorithms task 3

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,53 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        if (number < 0) return null
+        val numberPow: Long = 1L * number * number
+        val list = mutableListOf<Int>()
+        var x = 0
+        var pow = 0L
+        var powNext = 0L
+        var value = 0L
+        var indexFirst = 0
+        var indexSecond = 1
+        var correctionFirst = 1
+        var correctionSecond = 0
+        do {
+            do {
+                list.clear()
+                x = number - 1
+                list.add(x)
+                pow = 1L * x * x
+                value = numberPow - pow
+                while (value > 0) {
+                    x = sqrt(value.toDouble()).toInt()
+                    if (indexFirst == list.size)
+                        if (x > 2) x -= correctionFirst else break
+                    if (indexSecond == list.size)
+                        if (x > 2) x -= correctionSecond else break
+                    powNext = 1L * x * x
+                    if (powNext != pow && value >= powNext) {
+                        pow = powNext
+                        list.add(x)
+                        value -= pow
+                    } else break
+                }
+                if (value > 0) {
+                    if (indexFirst > 0) indexFirst-- else indexFirst = list.size / 2
+                }
+            } while (value > 0 && indexFirst > 0)
+            if (value > 0) {
+                indexFirst = list.size / 2
+                correctionSecond++
+            }
+        } while (value > 0 && indexFirst > 1 && correctionSecond <= 2)
+        return if (value == 0L) {
+            list.sort()
+            list.toTypedArray()
+        } else null
     }
 }
